d23b4c0c0c880506574d716614fa09ea9f9aa104
Chris Kaltwasser <chris@chrisk.com>
2021-10-07 12:41:53 -0400

    added jsonb_only option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use postgres::{error::Error, Client, NoTls};
 use serde_json::{Number, Value};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Activate jsonb_only mode
+    #[structopt(short = "j", long = "jsonb_only")]
+    jsonb_only: bool,
+}
 
 #[derive(Debug)]
 struct SaleWithProduct {
@@ -10,7 +19,7 @@ struct SaleWithProduct {
     date: i64,
 }
 
-fn create_db() -> Result<Client, Error> {
+fn create_db(opt: &Opt) -> Result<Client, Error> {
     let username = "chris";
     let password = "hello";
     let host = "localhost";
@@ -34,15 +43,26 @@ fn create_db() -> Result<Client, Error> {
 
     let _ = conn.execute("DROP TABLE Sales", &[]);
     let _ = conn.execute("DROP TABLE Products", &[]);
-    conn.execute(
-        "CREATE TABLE Products (
-            id INTEGER PRIMARY KEY,
-            category TEXT NOT NULL,
-            name TEXT NOT NULL UNIQUE,
-            sales_text text null,
-            sales_jsonb jsonb null)",
-        &[],
-    )?;
+    if opt.jsonb_only {
+        conn.execute(
+            "CREATE TABLE Products (
+                id INTEGER PRIMARY KEY,
+                category TEXT NOT NULL,
+                name TEXT NOT NULL UNIQUE,
+                sales jsonb null)",
+            &[],
+        )?;
+    } else {
+        conn.execute(
+            "CREATE TABLE Products (
+                id INTEGER PRIMARY KEY,
+                category TEXT NOT NULL,
+                name TEXT NOT NULL UNIQUE,
+                sales_text text null,
+                sales_jsonb jsonb null)",
+            &[],
+        )?;
+    }
     conn.execute(
         "CREATE TABLE Sales (
             id TEXT PRIMARY KEY,
@@ -79,20 +99,57 @@ fn grab_json_text_from_sample_file() -> String {
     serde_json::to_string(&sales_and_products).unwrap()
 }
 
-fn populate_db(conn: &mut Client) -> Result<(), Error> {
+fn grab_json_from_sample_file() -> serde_json::Value {
+    // Code below based on sample from "Creative Projects for Rust Programmers"
+    let input_path = "sales.json".to_string();
+
+    let mut sales_and_products = {
+        // Load the first file into a string.
+        let sales_and_products_text = std::fs::read_to_string(&input_path).unwrap();
+
+        // Parse the string into a dynamically-typed JSON structure.
+        serde_json::from_str::<Value>(&sales_and_products_text).unwrap()
+    };
+
+    // Get the field of the structure
+    // containing the weight of the sold oranges.
+    if let Value::Number(n) = &sales_and_products["sales"][1]["quantity"] {
+        // Increment it and store it back into the structure.
+        sales_and_products["sales"][1]["quantity"] =
+            Value::Number(Number::from_f64(n.as_f64().unwrap() + 1.5).unwrap());
+    }
+
+    // Save the JSON structure into the other file.
+    //serde_json::to_string(&sales_and_products).unwrap()
+    sales_and_products
+}
+
+fn populate_db(conn: &mut Client, opt: &Opt) -> Result<(), Error> {
 //    let sales_json = r#"{ "name" : "Julia" }"#;
-    let sales_json = grab_json_text_from_sample_file();
-    conn.execute(
-        "INSERT INTO Products (
-            id, category, name, sales_text
-            ) VALUES ($1, $2, $3, $4)",
-        &[&1, &"fruit", &"pears", &sales_json],
-    )?;
-    conn.execute(
-        "UPDATE Products 
-            SET sales_jsonb = cast (sales_text as jsonb)
-          WHERE sales_jsonb is null", &[]
-    )?;
+
+    if opt.jsonb_only {
+        let sales_json = grab_json_from_sample_file();
+        conn.execute(
+            "INSERT INTO Products (
+                id, category, name, sales
+                ) VALUES ($1, $2, $3, $4)",
+            &[&1, &"fruit", &"pears", &sales_json],
+        )?;
+    } else {
+        let sales_json_text = grab_json_text_from_sample_file();
+        conn.execute(
+            "INSERT INTO Products (
+                id, category, name, sales_text
+                ) VALUES ($1, $2, $3, $4)",
+            &[&1, &"fruit", &"pears", &sales_json_text],
+        )?;
+        conn.execute(
+            "UPDATE Products 
+                SET sales_jsonb = cast (sales_text as jsonb)
+              WHERE sales_jsonb is null", &[]
+        )?;
+    }
+
     conn.execute(
         "INSERT INTO Sales (
             id, product_id, sale_date, quantity, unit
@@ -135,8 +192,11 @@ fn print_db(conn: &mut Client) -> Result<(), Error> {
 }
 
 fn main() -> Result<(), Error> {
-    let mut conn = create_db()?;
-    populate_db(&mut conn)?;
+    let opt = Opt::from_args();
+    println!("opt = {:?}", opt);
+
+    let mut conn = create_db(&opt)?;
+    populate_db(&mut conn, &opt)?;
     print_db(&mut conn)?;
     Ok(())
 }
